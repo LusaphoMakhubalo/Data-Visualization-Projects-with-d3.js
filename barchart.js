355500b2f3e25f6327f82f35355caa14bdc99af0
Lusapho Makhubalo <111746512+LusaphoMakhubalo@users.noreply.github.com>
2023-06-01 22:12:53 +0200

    Update barchart.js

--- barchart.js
+++ barchart.js
@@ -1,4 +1,9 @@
-alert('Lusapho Makhubalo, is a self-taught Front-End-Developer');
+/*alert('Lusapho Makhubalo, is a self-taught Front-End-Developer');
         
 //greeting, as i am testing the linking of files.
-console.log('Hello, Lusapho Makhubalo!');
+console.log('Hello, Lusapho Makhubalo!');*/
+
+//Learning D3.js on FCC
+d3.select('body')
+.append('h1')
+.text('Learning D3');
